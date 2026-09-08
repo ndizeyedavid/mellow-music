278fbf2d79543cd74cc0cfb0352d842cbb571c11
ndizeyedavid <davidndizeye101@gmail.com>
2026-09-08 07:17:32 +0200

    ch: added a vercel analytics package to count wesbite views

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,13 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import "./index.css";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 NProgress.configure({ showSpinner: false });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
